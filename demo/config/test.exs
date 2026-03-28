549c35b931a6d5a4e1da2568c7c8ca881bf77c1d
Oliver Forral <intrepion@gmail.com>
2026-03-28 05:29:40 -0700

    just initial_create

---
+++ demo/config/test.exs
@@ -0,0 +1,28 @@
+import Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :demo, DemoWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "D+yBiaF89x8AKuihQHfDLnKGh7DzZK28zkHOM/2CaZ52+tr4c1Ih+l+spKIWgGN1",
+  server: false
+
+# In test we don't send emails
+config :demo, Demo.Mailer, adapter: Swoosh.Adapters.Test
+
+# Disable swoosh api client as it is only required for production adapters
+config :swoosh, :api_client, false
+
+# Print only warnings and errors during test
+config :logger, level: :warning
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
+
+# Enable helpful, but potentially expensive runtime checks
+config :phoenix_live_view,
+  enable_expensive_runtime_checks: true
+
+# Sort query params output of verified routes for robust url comparisons
+config :phoenix,
+  sort_verified_routes_query_params: true
